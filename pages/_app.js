3e3b1d4eee69cf8cb3e2a00e55fe8aa5d67c075b
Pushp Singh <pushp.singh@SW-LP02545.local>
2020-05-17 15:53:42 +0530

    dev for new timeline progressbar

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+import '../styles/global.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css'; 
+import 'mdbreact/dist/css/mdb.css';
+
+export default function App({Component, pageProps}) {
+    return <Component {...pageProps} />
+}
